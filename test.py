9e0ffe1a1c21d561cdf8c7b694e195398610927c
NearlyHeadlessJack <wang@rjack.cn>
2026-01-21 23:38:57 +0800

    update dependencies

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3.10
+# -*- coding: utf-8 -*-
+# author： NearlyHeadlessJack
+# email: wang@rjack.cn
+# datetime： 2026/1/21 23:37 
+# ide： PyCharm
+# file: test.py.py
+from imdb import Cinemagoer
+
+# create an instance of the Cinemagoer class
+ia = Cinemagoer()
+
+# get a movie
+movie = ia.get_movie('0133093')
+
+# print the names of the directors of the movie
+print('Directors:')
+for director in movie['directors']:
+    print(director['name'])
+
+# print the genres of the movie
+print('Genres:')
+for genre in movie['genres']:
+    print(genre)
+
+# search for a person name
+people = ia.search_person('Mel Gibson')
+for person in people:
+   print(person.personID, person['name'])
